437c37d87398383ea3be542a2d8ecb52e912804f
gokul <gokul.sridharan@blackcatsolutions.co.uk>
2026-02-23 22:19:58 +0000

    fix for api test stage

--- lib/env.ts
+++ lib/env.ts
@@ -1,10 +1,10 @@
 export class ENV {
 
     //API creds
-    public static API_VALID_USERNAME = process.env.API_VALID_USERNAME;
-    public static API_VALID_PASSWORD = process.env.API_VALID_PASSWORD;
-    public static API_INVALID_USERNAME = process.env.API_INVALID_USERNAME;
-    public static API_INVALID_PASSWORD = process.env.API_INVALID_PASSWORD;
+    public static get API_VALID_USERNAME() { return process.env.API_VALID_USERNAME; }
+    public static get API_VALID_PASSWORD() { return process.env.API_VALID_PASSWORD; }
+    public static get API_INVALID_USERNAME() { return process.env.API_INVALID_USERNAME; }
+    public static get API_INVALID_PASSWORD() { return process.env.API_INVALID_PASSWORD; }
 
     //UI creds
     public static UI_VALID_USERNAME = process.env.UI_VALID_USERNAME;
